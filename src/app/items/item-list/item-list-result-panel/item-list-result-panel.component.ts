48eb3597459a186e7da1d3c8d7904bdfec200afe
tiamatt <samira.yusifova@gmail.com>
2017-11-27 23:52:43 -0500

    Modify ItemListComponent. Move table panel to separate component (ItemListResultPanelComponent)

--- src/app/items/item-list/item-list-result-panel/item-list-result-panel.component.ts
+++ src/app/items/item-list/item-list-result-panel/item-list-result-panel.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ItemViewClass } from '../../../shared/models/ItemView.model';
 
 @Component({
   selector: 'app-item-list-result-panel',
   templateUrl: './item-list-result-panel.component.html',
   styleUrls: ['./item-list-result-panel.component.css']
 })
-export class ItemListResultPanelComponent implements OnInit {
 
+export class ItemListResultPanelComponent implements OnInit {
+  tableData: ItemViewClass[];
+  isSortAsc: boolean = true;
+  pages: number[] = [1];
+  currentPage: number = 1;
+  currentSortedTableHeaderName: string = "name";
+  readonly maxRowsPerPage:number = 5;
+
+  @Input() input: ItemViewClass[];
+  
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { 
+    // initial paging and sorting
+    this.sortingAndPagingTable(this.currentSortedTableHeaderName);
+    
+    // get total numbers of pages
+    let totalNumberOfPages = Math.ceil(this.input.length / this.maxRowsPerPage);
+    console.log(totalNumberOfPages);
+    for(var i = 2; i<= totalNumberOfPages; i++){
+      this.pages.push(i);
+    }
+
+  }
+
+
+  /* -------------------  EVENTS ---------------------- */
+  onSort(_tableHeaderName: string){
+    this.currentSortedTableHeaderName = _tableHeaderName;    
+    this.isSortAsc = !this.isSortAsc;  // change sort direction to opposite
+    this.currentPage = 1;   // make first page as current page
+    this.sortingAndPagingTable(_tableHeaderName);
+  }
+  onPageChange(_currentPage: number){
+    this.currentPage = _currentPage;    
+    this.sortingAndPagingTable(this.currentSortedTableHeaderName);
+  }
+
+
+  /* -------------------- PRIVATE METHODS -------------------------- */
+  private sortingAndPagingTable(_tableHeaderName: string){
+    // sort asc
+    if(this.isSortAsc == true){
+      this.tableData = this.input.sort(function(a, b) {
+            return a.item[_tableHeaderName] > b.item[_tableHeaderName]  ? 1 : ((a.item[_tableHeaderName] < b.item[_tableHeaderName] ) ? -1 : 0);
+      });
+    }  
+    // sort desc
+    else {
+      this.tableData = this.input.sort(function(a, b) {
+        return a.item[_tableHeaderName] < b.item[_tableHeaderName]  ? 1 : ((a.item[_tableHeaderName] > b.item[_tableHeaderName] ) ? -1 : 0);
+      });
+    }
+    // slicing to show records less than maxRowsPerPage
+    let fromRow = (this.currentPage - 1) * this.maxRowsPerPage;
+    let toRow = this.currentPage*this.maxRowsPerPage;
+    this.tableData = this.tableData.slice(fromRow, toRow);
   }
 
 }
--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -8,15 +8,10 @@ import { ItemListFilterPanelClass } from '../../shared/models/ItemListFilterPane
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css', '../../shared/styles/general.css']
 })
+
 export class ItemListComponent implements OnInit {
   itemListFilterPanel: ItemListFilterPanelClass;
-  itemViews: ItemViewClass[] = [];
-  tableData: ItemViewClass[];
-  isSortAsc: boolean = true;
-  pages: number[] = [1];
-  currentPage: number = 1;
-  currentSortedTableHeaderName: string = "name";
-  readonly maxRowsPerPage:number = 5;
+  itemViewsTableData: ItemViewClass[];
 
   constructor(
     private itemApiService: ItemApiService
@@ -40,15 +35,8 @@ export class ItemListComponent implements OnInit {
   populateItemViews(){
     this.itemApiService.getItemViews().subscribe(
       res => { 
-        this.itemViews = res;
-        this.sortingAndPagingTable(this.currentSortedTableHeaderName); 
-
-        let totalNumberOfPages = Math.ceil(this.itemViews.length / this.maxRowsPerPage);
-        console.log(totalNumberOfPages);
-        for(var i = 2; i<= totalNumberOfPages; i++){
-          this.pages.push(i);
-        }
-
+        this.itemViewsTableData = res;
+        console.log(this.itemViewsTableData);
       },
       err => console.log(err)
     );
@@ -61,37 +49,6 @@ export class ItemListComponent implements OnInit {
   }
 
   
-  onSort(_tableHeaderName: string){
-    this.currentSortedTableHeaderName = _tableHeaderName;    
-    this.isSortAsc = !this.isSortAsc;  // change sort direction to opposite
-    this.currentPage = 1;   // make first page as current page
-    this.sortingAndPagingTable(_tableHeaderName);
-  }
-  onPageChange(_currentPage: number){
-    this.currentPage = _currentPage;    
-    this.sortingAndPagingTable(this.currentSortedTableHeaderName);
-  }
-
-
-
-  /* -------------------- PRIVATE METHODS -------------------------- */
-  private sortingAndPagingTable(_tableHeaderName: string){
-    // sort asc
-    if(this.isSortAsc == true){
-      this.tableData = this.itemViews.sort(function(a, b) {
-            return a.item[_tableHeaderName] > b.item[_tableHeaderName]  ? 1 : ((a.item[_tableHeaderName] < b.item[_tableHeaderName] ) ? -1 : 0);
-      });
-    }  
-    // sort desc
-    else {
-      this.tableData = this.itemViews.sort(function(a, b) {
-        return a.item[_tableHeaderName] < b.item[_tableHeaderName]  ? 1 : ((a.item[_tableHeaderName] > b.item[_tableHeaderName] ) ? -1 : 0);
-      });
-    }
-    // slicing to show records less than maxRowsPerPage
-    let fromRow = (this.currentPage - 1) * this.maxRowsPerPage;
-    let toRow = this.currentPage*this.maxRowsPerPage;
-    this.tableData = this.tableData.slice(fromRow, toRow);
-  }
+  
 
 }
